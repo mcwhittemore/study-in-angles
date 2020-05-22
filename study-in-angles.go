0f9c07f2c11828312a0d2f1cbb406e5fc711799f
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2020-05-21 22:17:01 -0400

    update to runner based pixicog-cli

--- study-in-angles.go
+++ study-in-angles.go
@@ -1,67 +1,71 @@
 package main
 
 import (
-  "image"
-  "os"
-  "image/color"
-  "github.com/mcwhittemore/pixicog"
+	"encoding/base64"
+	"fmt"
+	"github.com/mcwhittemore/pixicog"
+	"image"
+	"image/color"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func ProcessLoad(src, state pixicog.ImageList) (pixicog.ImageList, pixicog.ImageList) {
-  src, err := pixicog.ImageListFromVideoFileName(os.Args[1])
-  if err != nil {
-    panic(err)
-  }
-
-  state = pixicog.ImageList{}
-  src = src.Rotate(90)
-
-  return src, state
+func (r *Runner) ProcessLoad() {
+	src, err := pixicog.ImageListFromVideoFileName(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	src = src.Rotate(90)
+	r.state["src"] = src
 }
 
-func ProcessFirstLayer(src, state pixicog.ImageList) (pixicog.ImageList, pixicog.ImageList) {
-  width := src.Width()
-  height := src.Height()
+func (r *Runner) ProcessFirstLayer() {
+	src := r.state["src"]
 
-  img := image.NewRGBA(src.Bounds())
+	width := src.Width()
+	height := src.Height()
 
-  for x := 0; x < width; x++ {
-    for y := 0; y < height; y++ {
-      colors := src.GetDiminished(x,y,16)
-      c := mostCommon(colors)
-      img.Set(x, y, c)
-    }
-  }
+	img := image.NewRGBA(src.Bounds())
 
-  state = append(state, img)
-  return src, state
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			colors := src.GetDiminished(x, y, 16)
+			c := mostCommon(colors)
+			img.Set(x, y, c)
+		}
+	}
+
+	state := pixicog.ImageList{}
+	state = append(state, img)
+
+	r.state["src"] = state
 }
 
-func ProcessSave(src, state pixicog.ImageList) (pixicog.ImageList, pixicog.ImageList) {
-  state.SavePNG(os.Args[2])
-  return src, state
+func (r *Runner) ProcessSave() {
+	r.state["src"].SavePNG(os.Args[2])
 }
 
 func mostCommon(colors []color.Color) color.Color {
-  common := colors[0]
-  commonCount := 0
+	common := colors[0]
+	commonCount := 0
 
-  n := len(colors)
+	n := len(colors)
 
-  for i := 0; i < n; i++ {
-    t := colors[i]
-    tc := 0
-    for j := i + 1; j < n; j++ {
-      if t == colors[j] {
-        tc++
-      }
-    }
-    if tc > commonCount {
-      commonCount = tc
-      common = t
-    }
-  }
+	for i := 0; i < n; i++ {
+		t := colors[i]
+		tc := 0
+		for j := i + 1; j < n; j++ {
+			if t == colors[j] {
+				tc++
+			}
+		}
+		if tc > commonCount {
+			commonCount = tc
+			common = t
+		}
+	}
 
-  return common
+	return common
 }
-
